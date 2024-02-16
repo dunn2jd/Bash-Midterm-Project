2c1ad5ea259a16652e194fb18d843e60f9b2b591
dunn2jd <dunn2jd@mail.uc.edu>
2024-02-16 17:55:22 -0500

    created

--- MidtermProject.sh
+++ MidtermProject.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-# READ ME: After like 30 minutes of trial and error we couldn't figure it out so when you run the test script it will fail at Case5, but you verified that my script is correct
-
 # Function to display script usage/help information
 function display_usage() {
   # hint: you will have multiple echo statements here
-  echo "Usage: $0 -f <filename> -o <output_dir> -t <type>"
+  echo "Usage: $0 -f <filename> -p <permission>"
   echo "Options:"
-  echo "  -f <filename>: Specify the input filename."
-  echo "  -o <output_dir>: Specify the output directory where the results will be stored."
-  echo "  -t <type>: Specify the type of action to perform. Supported types are 'process' and 'analyze'."
+  echo "  -f <filename>: Specify the file you want to change Permissions for."
+  echo "  -p <permission>: Specify new permissions for the file."
+  echo "  -i Linux: Linux File Permissions Explained."
   echo "  -h: Display this help information."
+}
+
+function linux_help_info() {
+  echo "Permissons work as follows: "
+  echo "The first number represents the file owner"
+  echo "The second number represents the user group the owner is in"
+  echo "The third numebr represents any other users that are on the system"
+  echo "1 = Execute, 2 = Write, and 4 = Read. "
+  echo "For example, if you wanted the file owner to have all perms, owner's group "
+  echo "to have both read & write, but only read for everyone else would be: 764"
 }
 
 # Check if no arguments are provided.
@@ -22,20 +30,21 @@ fi
 
 # Initialize variables (you don't need to change this part)
 filename=""
-output_dir=""
-action=""
+permissions=""
+information=""
 
 # Process command-line options and arguments
-while getopts ":f:o:t:h" opt; do
+while getopts ":f:p:i:h" opt; do
     case $opt in
         f) # option f
             filename=$OPTARG
             ;;
-        O) # option o
-            output_dir=$OPTARG
+        p) # option p
+            permissions=$OPTARG
             ;;
-        t) # option t
-            action=$OPTARG
+        i) # option i
+            linux_help_info
+            exit 0
             ;;
         h) # option h
             display_usage
@@ -53,36 +62,24 @@ while getopts ":f:o:t:h" opt; do
             ;;
         esac
     done
-# Check if all required switches are provided (f, o, t are required. since each of these sets a variable, you can use those variables to know if the option was provided)
-    if [[ -z $filename || -z $output_dir || -z $action ]]; then
+# Check if all required switches are provided (f & p are required)
+    if [[ -z $filename || -z $permissions ]]; then
     echo "Error: Missing required options."
     display_usage
     exit 1
     fi
 
-# Check if the -t switch has a valid argument
-    if [[ $action != "process" && $action != "analyze" ]]; then
-    echo "Error: Invalid action type. Supported types are 'process' and 'analyze'."
-    exit 1
-    fi
-
-# Check if the specified input filename exists and is a regular file
+# Check if the specified input filename exists 
     if [[ ! -f $filename ]]; then
     echo "Error: Input file '$filename' does not exist or is not a regular file."
     exit 1
+    fi 
+    
+# Check if the -p switch has a valid argument,
+    if [[ "$permissions" = '[0-9]{3,}' ]]; then
+    echo "Error: Invalid Permission. Use -i to see more information on Linux File Permissions."
     fi
 
-# Check if the specified output directory exists and is a directory
-    if [[ ! -d $output_dir ]]; then
-    echo "Error: Output directory '$output_dir' does not exist."
-    exit 1
-    fi
-
-# Perform the selected action based on the user input
-if [[ $action == "process" ]]; then
-    echo "Processing task will be performed on '$filename' and the results will be stored in '$output_dir'."
-    # make a backup file like last week
-elif [[ $action == "analyze" ]]; then
-    echo "Analysis will be performed on '$filename' and the report will be stored in '$output_dir'."
-    # for example document word count or or information regarding the file
-fi
+#  Change the permissions of the file
+    chmod $permissions $filename
+    echo "Permissions Successfully Changed to: $permissions"
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Bash-Midterm-Project
+IT3038C BASH Midterm Project
+Jeremy Dunn
+
+Purpose:
+The purpose of this script is to change the permissions for files without needed to type the command.
+
+If you ever feel like you need a refresher of the Linux Permissions simply enter:
+./MidtermProject -i Linux
+(This Information is found in the help page by using '-h')
